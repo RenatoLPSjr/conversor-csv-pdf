ccd14f8b36d3d8effcc2e33345395b7e0b47b08f
Renato Lopes Junior <89666395+RenatoLPSjr@users.noreply.github.com>
2023-01-16 22:17:14 -0300

    Adicionando tableas e processos

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var Reader = require("./Reader");
+var Processor = require("./Processor");
+var Table = require("./Table");
+
 
 var leitor = new Reader();
 
 
 async function main(){
     var dados = await leitor.Read("./usuarios.csv");
-    console.log(dados)
+    var dadosProcessados = Processor.Process(dados);
+    var usuarios= new Table(dadosProcessados);
+
+
+
 }
+
+main()
